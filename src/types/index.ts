a839161b28ac530b31d133f37e36e9be863c266d
mauricio rojas díaz <freman226@gmail.com>
2025-04-17 21:51:05 -0400

    second commit

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Guitar = {
+    id: number
+    name: string
+    image: string
+    description: string
+    price: number
+}
+
+export type CartItem = Guitar & {
+    quantity: number 
+}
+
+// export type GuitarID = Guitar['id']
+
+// export type CartItem2 = Pick <Guitar, 'id' | 'name' | 'price'> & {
+//     quantity: number
+// }
+
+// export type CartItem3 = Omit <Guitar, 'id' | 'name' | 'price'> & {
+//     quantity: number
+// }
+
